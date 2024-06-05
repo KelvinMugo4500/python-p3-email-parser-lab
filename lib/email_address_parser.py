3754a38b2090c853f80d8154d9c89013b831f860
Kelvin Mugo Maina <kelvinmugo912@gmail.com>
2024-06-05 12:56:57 +0300

    initial commit

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,15 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_addresses):
+        self.email_addresses = email_addresses
+
+    def parse(self):
+        
+        pattern = r'[\w\.-]+@[\w\.-]+'
+        addresses = re.findall(pattern, self.email_addresses)
+
+        
+        unique_addresses = sorted(set(addresses))
+
+        return unique_addresses
